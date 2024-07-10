6d1530ca32a77830aad9432e2a8b330512bea953
jair-chaguay <jchaguay@espol.edu.ec>
2024-07-10 14:22:15 -0500

    update

--- routes/api.js
+++ routes/api.js
@@ -4,8 +4,8 @@ const itemController = require('../controllers/itemController');
 
 router.post('/items', itemController.createItem);
 router.get('/items', itemController.getAllItems);
-router.getById('/items/:id', itemController.getItem);
-router.update('/items/:id', itemController.updateItem);
-router.getById('/items/:id', itemController.deleteItem);
+router.get('/items/:id', itemController.getItem);
+router.put('/items/:id', itemController.updateItem);
+router.delete('/items/:id', itemController.deleteItem);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,11 @@ require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const admin = require('firebase-admin');
+ /* Referencia al módulo swagger-ui-express */
+ const swaggerUi = require('swagger-ui-express')
+
+ /* Referencia al archivo con la descripción */
+ const swaggerFile = require('./swagger_output.json')
 const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_API)
 
 admin.initializeApp({
@@ -13,6 +18,9 @@ const app = express();
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
+
+
+app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use('/api', require('./routes/api'));
 
