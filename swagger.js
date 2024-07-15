6d318ab68c66ece6c80221813b4b5e0beb37acda
jair-chaguay <jchaguay@espol.edu.ec>
2024-07-15 13:48:43 -0500

    Update

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const swaggerAutogen = require('swagger-autogen')()
      "title": 'REST API',
      "description": 'REST API with Express and Firestore'
    },
-   "host": 'localhost:5500',
+   "host": 'https://restapi-qzek.onrender.com',
    "basePath": "/api",
  };
 
